1a6f9615c9d50218605f97927cae30b81806642b
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-03-25 17:19:17 -0400

    Add Package.swift for Swift 6.0

---
+++ Package@6.0.0.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MultiformatsKit",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v12),
+        .tvOS(.v14),
+        .visionOS(.v1),
+        .watchOS(.v9)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MultiformatsKit",
+            targets: ["MultiformatsKit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.12.2")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MultiformatsKit",
+            dependencies: [
+                .product(name: "Crypto", package: "swift-crypto")
+            ]),
+        .testTarget(
+            name: "MultiformatsKitTests",
+            dependencies: ["MultiformatsKit"]
+        ),
+    ]
+)
