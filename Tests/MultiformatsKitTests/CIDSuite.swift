35f2ad53352b0c9eae9a842118aa0cf300e6c01b
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-03-25 19:35:38 -0400

    Add unit test for CID

---
+++ Tests/MultiformatsKitTests/CIDSuite.swift
@@ -0,0 +1,25 @@
+//
+//  CIDSuite.swift
+//  MultiformatsKit
+//
+//  Created by Christopher Jr Riley on 2025-03-25.
+//
+
+import Foundation
+import Testing
+@testable import MultiformatsKit
+
+@Suite("CID Suite") struct CIDSuite {
+    @Test("Generates, then round trips between encoding and decoding a CIDv1.") func cidRoundtrip() async throws {
+        let text = "I'm feeling great today!"
+
+        let cid = try await CID(content: text)
+
+        let encodedCID = cid.encode()
+        try #require(encodedCID == "bafybeifhhzii2au6jnkhjr3ng3r5s7pn3td7xzbos547rlqonpgc76fde4", "The encoded string should match the original text.")
+
+        let decoded = try await CID.decode(from: cid.encode())
+
+        #expect(decoded == cid, "The decoded CID should match the original CID.")
+    }
+}
--- Tests/MultiformatsKitTests/MultibaseSuite.swift
+++ Tests/MultiformatsKitTests/MultibaseSuite.swift
@@ -12,15 +12,19 @@ import Testing
 @Suite("Multibase Suite") struct MultibaseSuite {
 
     @Suite("BaseX Tests") struct BaseXTests {
-        @Test("Roundtrips between decoding and encoding base58btc.")
+        @Test("Roundtrips between decoding and encoding base32.")
         func base32RoundTrip() async throws {
+            guard let text = "I'm feeling great today!".data(using: .utf8) else { return }
             let baseEncoder = BaseCodec.base32Lower
-            let encodedResult = try baseEncoder.decode("I'm feeling great today!")
+            let encodedResult = baseEncoder.encode(text)
 
-            try #require(encodedResult == Data([0x4a, 0x45, 0x54, 0x57, 0x32, 0x49, 0x44, 0x47, 0x4d, 0x56, 0x53, 0x57, 0x59, 0x32, 0x4c, 0x4f, 0x4d, 0x34, 0x51, 0x47, 0x4f, 0x34, 0x54, 0x46, 0x4d, 0x46, 0x32, 0x43, 0x41, 0x35, 0x44, 0x50, 0x4d, 0x52, 0x51, 0x58, 0x53, 0x49, 0x49]), "The result should be equal to the original data.")
+            try #require(encodedResult == "jetw2idgmvswy2lom4qgo4tfmf2ca5dpmrqxsii", "The result should be equal to the original data.")
 
-            let decodedResult = baseEncoder.encode(encodedResult)
-            #expect(decodedResult == "I'm feeling great today!", "The encoded string should be equal to the original decoded string.")
+            let decodedResult = try baseEncoder.decode(encodedResult)
+            guard let decodedString = String(data: decodedResult, encoding: .utf8) else {
+                return
+            }
+            #expect(decodedString == "I'm feeling great today!", "The result should be equal to the original text.")
         }
     }
 }
