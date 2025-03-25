a0e6b406bfdbad13301628ce9bf1025e5922db3e
Christopher Jr Riley <chrisjr@cjrriley.com>
2025-03-25 16:05:12 -0400

    Add platforms in Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,11 @@ import PackageDescription
 let package = Package(
     name: "MultiformatsKit",
     platforms: [
-        .macOS(.v12)
+        .iOS(.v14),
+        .macOS(.v12),
+        .tvOS(.v14),
+        .visionOS(.v1),
+        .watchOS(.v9)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
